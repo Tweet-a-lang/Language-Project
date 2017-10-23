b7a9e50b3c25cd171cddf5348dbe0bdc4db0c427
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-10-23 15:56:31 +0100

    build game data and add tweet and username

--- src/components/GamePage/GameCard.js
+++ src/components/GamePage/GameCard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import GameHint from './GameHint';
-
+import Data from '../../../public/data/muyinteresante.json';
 
 class GameCard extends React.Component {
     render() {
         return (
             <div>
-                <p>Olie</p>
-                <span>Image here</span>
-
-                <p>Un ejemplo en espagnol</p>
+                <p>Player: Olie</p>
+                <span>Player's Profile Image here</span>
+{console.log(Data)}
+                <h3>@{Data[80].user.screen_name}</h3>
+                <p>{Data[80].text}</p>
+                
                 <ul>
                     <li>example</li>
                     <li>test</li>
