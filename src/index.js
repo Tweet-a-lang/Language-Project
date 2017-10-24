97e87fe0b086a7d896639156fbae9747b7b94bf4
Rosie Armstrong <rosemary_a@live.co.uk>
2017-10-24 14:51:19 +0100

    implement redux store and reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 import App from './components/App';
 
-render(
-    <App />,
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import enterHandle from './enterHandle'
+
+const reducer = combineReducers({
+    enterHandle
+})
+
+export default reducer;
