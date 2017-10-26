ca7d018d04c1da12d780816392f8888a8053c86d
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-10-26 17:40:02 +0100

    implement and test for fetchLeaderboard action creator

---
+++ src/actions/fetchLeaderboard.js
@@ -0,0 +1,31 @@
+import * as types from './types';
+import axios from 'axios';
+
+const API_URL = 'http://192.168.100.33:3001/api';
+
+export const fetchLeaderboardRequest = () => ({
+  type: types.FETCH_LEADERBOARD_REQUEST
+});
+
+export const fetchLeaderboardSuccess = (data) => ({
+  type: types.FETCH_LEADERBOARD_SUCCESS,
+  payload: data
+});
+
+export const fetchLeaderboardFailure = (error) => ({
+  type: types.FETCH_LEADERBOARD_FAILURE,
+  payload: error
+});
+
+export default () => {
+  return (dispatch) => {
+    dispatch(fetchLeaderboardRequest());
+    return axios.get(`${API_URL}/scoreboard`)
+      .then(res => {
+        dispatch(fetchLeaderboardSuccess(res.data));
+      })
+      .catch(err => {
+        dispatch(fetchLeaderboardFailure(err.message));
+      });
+  };
+};
