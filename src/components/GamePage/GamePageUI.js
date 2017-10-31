5fa32daa25848c8f4920443accef067697d6e993
Dennis Foster <contact@DennisFoster.co.uk>
2017-10-31 17:18:27 +0000

    Adding styling for gamepage

--- src/components/GamePage/GamePageUI.js
+++ src/components/GamePage/GamePageUI.js
@@ -5,42 +5,49 @@ import CorrectPopUp from './Results/CorrectPopUp';
 import InCorrectPopUp from './Results/InCorrectPopUp';
 import GameScore from '../../containers/GamePage/GameScore';
 import _ from 'underscore';
+import '../../css/gamepage.css';
 
 let tweetDisabledArr = [];
 
 const GamePageUI = ({ username, tweetArr, onCorrect, onIncorrect, modalCorrectIsOpen, modalInCorrectIsOpen, closeModal, tweet0, tweet1, tweet2, tweet3, tweet4 }) => (
 
   <div>
-    <p>Player: {username}</p>
-    <GameScore />
-    {tweetArr.map((tweetData, tweetIndex) => {
-      return (<div key={tweetIndex}>
-        <h5>Tweets from: @{tweetData.tweet.user_screen_name}</h5>
+    <div className='user-container'>
+      <div className='user-ui'>
+        <p>Player: {username}</p>
+        <GameScore />
+      </div>
+    </div>
+    <div className='tweet-container'>
+      {tweetArr.map((tweetData, tweetIndex) => {
+        return (<div key={tweetIndex} className='tweet'>
+          <h5>Tweets from: @{tweetData.tweet.user_screen_name}</h5>
 
-        {parseTweet(tweetData)}
+          {parseTweet(tweetData)}
 
-        {tweetDisabledArr = [tweet0, tweet1, tweet2, tweet3, tweet4]}
+          {tweetDisabledArr = [tweet0, tweet1, tweet2, tweet3, tweet4]}
 
-        <div>
-          {_.shuffle(tweetData.answers.choices).map((choice, buttonIndex) => {
-            return <button key={buttonIndex} className='choice-button' type='submit' value={[tweetData.tweet.id, tweetIndex]} disabled={tweetDisabledArr[tweetIndex]} onClick={(choice.result) ? onCorrect : onIncorrect}>{choice.text}</button>;
-          })}
+          <div>
+            {_.shuffle(tweetData.answers.choices).map((choice, buttonIndex) => {
+              return <button key={buttonIndex} className='choice-button' type='submit' value={[tweetData.tweet.id, tweetIndex]} disabled={tweetDisabledArr[tweetIndex]} onClick={(choice.result) ? onCorrect : onIncorrect}>{choice.text}</button>;
+            })}
 
-          <HintSelection
-            word={tweetData.answers.choices[0]}
-            disabled={tweetDisabledArr[tweetIndex]}
-            dictionaryHint={tweetData.answers.hints} />
-            
-          <CorrectPopUp 
-            closeModal={closeModal} 
-            modalCorrectIsOpen={modalCorrectIsOpen} 
-            correctIndex={tweetIndex} />
+            <HintSelection
+              word={tweetData.answers.choices[0]}
+              disabled={tweetDisabledArr[tweetIndex]}
+              dictionaryHint={tweetData.answers.hints} />
 
-          <InCorrectPopUp 
-            closeModal={closeModal} 
-            modalInCorrectIsOpen={modalInCorrectIsOpen} /></div>
-      </div>);
-    })}
+            <CorrectPopUp
+              closeModal={closeModal}
+              modalCorrectIsOpen={modalCorrectIsOpen}
+              correctIndex={tweetIndex} />
+
+            <InCorrectPopUp
+              closeModal={closeModal}
+              modalInCorrectIsOpen={modalInCorrectIsOpen} /></div>
+        </div>);
+      })}
+    </div>
   </div>
 );
 
