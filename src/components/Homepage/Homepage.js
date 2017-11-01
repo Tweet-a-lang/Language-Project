15d5283949314886a529d71d39480715f16fdf86
Dennis Foster <contact@DennisFoster.co.uk>
2017-11-01 19:06:31 +0000

    Adding homepage styling

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Start from '../../containers/Homepage/Start';
 import Leaderboard from '../../containers/Homepage/Leaderboard';
 import HowToPlay from './/HowToPlay';
+import '../../css/app.css';
 
 class Homepage extends React.Component {
   render() {
     return (
-      <div>
-        <Start />
-        <Leaderboard />
-        <HowToPlay />
+      <div className='rows'>
+        <div className='columns'>
+          <h1 className='gameHeader column is-four-fifths'>TWEET-A-LANG</h1>
+          <Start/>
+        </div>
+        <div className='row'>
+          <Leaderboard />
+          <HowToPlay />
+        </div>
       </div>
     );
   }
