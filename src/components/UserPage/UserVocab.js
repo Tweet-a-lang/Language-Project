bf745131e8e6bc60678873b1c6da943f41d915ac
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-11-01 16:27:14 +0000

    implement uservocab in components

---
+++ src/components/UserPage/UserVocab.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PT from 'prop-types';
+import { connect } from 'react-redux';
+
+
+class UserVocab extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>My Vocab</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Spanish</th>
+              <th>English</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.vocab.map((wordPair, i) => {
+              return (
+                <tr key={i}>
+                  <td key={wordPair.Spanish}>{wordPair.Spanish}</td>
+                  <td key={wordPair.English}>{wordPair.English}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+
+  }
+}
+
+UserVocab.propTypes = {
+  vocab: PT.array
+};
+
+
+const mapStateToProps = (state) => {
+  return {
+    vocab: state.userReducer.userData.vocab
+  };
+};
+
+
+export default connect(mapStateToProps)(UserVocab);
