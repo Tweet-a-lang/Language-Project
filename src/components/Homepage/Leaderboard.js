b96822184af434a0e7e9146ad0935c39fe397440
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-11-01 15:44:56 +0000

    leaderboard image and name links to the user page

--- src/components/Homepage/Leaderboard.js
+++ src/components/Homepage/Leaderboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PT from 'prop-types';
-
+import {Link} from 'react-router-dom';
 const leaderBoardStyle = {
   'borderRadius': '50%',
   
@@ -21,8 +21,14 @@ const LeaderboardUI = ({data}) => (
         {data.map((user, i) => {
           return (
             <tr key={i}>
-              <td key={user.avatar}><img src={user.avatar} style={leaderBoardStyle} width='50' height='50'/></td>
-              <td key={user.name}>{user.name}</td>
+              <td key={user.avatar}>
+                <Link to ={ `/user/${user.name}`}>
+                  <img src={user.avatar} style={leaderBoardStyle} width='50' height='50'/>
+                </Link>
+              </td>
+              <Link to ={ `/user/${user.name}`}>
+                <td key={user.name}>{user.name}</td>
+              </Link>
               <td key={user.name + user.score}>{user.score}</td>
             </tr>
           );
