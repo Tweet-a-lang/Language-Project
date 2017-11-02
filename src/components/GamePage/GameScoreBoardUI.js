db4df4125796caa31e7e04f663324c14fcae5412
Rosie Armstrong <rosemary_a@live.co.uk>
2017-11-02 15:34:12 +0000

    style scoreboard

--- src/components/GamePage/GameScoreBoardUI.js
+++ src/components/GamePage/GameScoreBoardUI.js
@@ -5,8 +5,10 @@ import '../../css/gamepage.css';
 
 const GameScoreBoardUI = ({username, usernameParams, gameScore}) => (
   <div className="game-scoreboard">
-    <p>PLAYER: {(username) ? username : usernameParams }</p>
-    <p>SCORE: {gameScore}</p>
+    <div className="user-info">
+      <p className="head">PLAYER:</p><p className="info">{(username) ? username : usernameParams }</p>
+      <p className="head">SCORE:</p><p className="info">{gameScore}</p>
+    </div>
   </div>
 
 );
