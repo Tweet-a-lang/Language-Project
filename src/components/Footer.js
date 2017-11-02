a3a9de692a4bb665d6dedef6afa4f9023a94fa46
Dennis Foster <contact@DennisFoster.co.uk>
2017-11-02 13:47:25 +0000

    Adding github links on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,43 @@ import '../css/footer.css';
 import _ from 'underscore';
 
 const Footer = () => {
-  const names = ['Dennis Foster', 'Rosie Armstrong', 'Kerry Godsmark', 'Olie Chan'];
-  const randomOrderNames = _.sample([0,1,2,3], 4).map((name, i) => {
-    if (i === 3) return 'and ' + names[name];
-    return names[name];
+  const names = [
+    {
+      name: 'Dennis Foster',
+      gitHub: 'https://github.com/Sinnedennis'
+    },
+    {
+      name: 'Rosie Armstrong',
+      gitHub: 'https://github.com/rosiearms'
+    },
+    {
+      name: 'Kerry Godsmark',
+      gitHub: 'https://github.com/kgodsmark'
+    },
+    {
+      name: 'Olie Chan',
+      gitHub: 'https://github.com/ochan7'
+    },
+  ];
+  const randomOrderNames = _.sample([0, 1, 2, 3], 4).map((nameIndex, i) => {
+    if (i === 3) {
+      return (
+        <a href={names[nameIndex].gitHub} key={i}>and {names[nameIndex].name} </a>
+      );
+    }
+    return (
+      <a href={names[nameIndex].gitHub} key={i}> {names[nameIndex].name}, </a>
+    );
   });
 
   const currentYear = new Date().getFullYear();
   return (
     <div className='footer'>
-      <p>Created by: {randomOrderNames.join(', ')}</p>
+      <p>Created by: {randomOrderNames}</p>
       <p className='copyright'>© Tweetalang {currentYear}</p>
     </div>
   );
 };
 
 export default Footer;
+
