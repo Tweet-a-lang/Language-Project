eeaa16dac6e4ea0afb373871494a29fea0f831f3
Kerry Godsmark <kerry.godsmark@gmail.com>
2017-12-06 15:18:04 +0000

    refactor webpack config to allow deploy to heroku

---
+++ webpack.dev.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const PATHS = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  public: path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'src')
+};
+
+module.exports = {
+  devtool: 'eval',
+
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: PATHS.src,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'env']
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js?$/,
+        loader: 'babel',
+        exclude: path.join(__dirname, 'node_modules')
+      },
+      {
+        test: /\.css?$/,
+        loader: 'style-loader!css-loader!sass-loader',
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.png$/,
+        loader: 'file'
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
+    ]
+  }
+
+};
